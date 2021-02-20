0da36af267dc72464114b089c76c1523f578c17d
Francisco J. López <frandufour@protonmail.com>
2021-02-20 00:17:34 +0000

    Prueba relative path in css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DAW2  Login</title>
-    <link rel="stylesheet" href="{{ asset('styles\bootstrap-4.0.0\css\bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('.\styles\bootstrap-4.0.0\css\bootstrap.min.css') }}">
 </head>
 <body>
     <div class="container">
