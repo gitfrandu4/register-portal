1d8b154922e9535c288443d6d018bf0253cf2393
Francisco J. López <frandufour@protonmail.com>
2021-02-20 00:25:15 +0000

    correcciones

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DAW2  Login</title>
-    <link rel="stylesheet" href="{{ asset('.\styles\bootstrap-4.0.0\css\bootstrap.min.css') }}">
+    <link rel="stylesheet" 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
+          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" 
+          crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
